b4bd8cc907ad2e835b2229cd12a49701b7f7c877
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-12 11:54:01 +0200

    Connect to db in MongoDB Atlas with mongoose and connection uri in .env

--- server/app.js
+++ server/app.js
@@ -1,20 +1,38 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const express = require("express");
+const path = require("path");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const mongoose = require("mongoose");
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+require("dotenv").config();
 
-var app = express();
+const indexRouter = require("./routes/index");
+const usersRouter = require("./routes/users");
 
-app.use(logger('dev'));
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+const uri = process.env.ATLAS_URI;
+mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const connection = mongoose.connection;
+connection.on("open", () => {
+	console.log("MongoDB database connection established successfully!");
+});
+connection.on("error", (err) => {
+	console.log(`Error connecting to MongoDB: ${err}`);
+});
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+
+app.listen(port, () => {
+	console.log(`Server is running on port: ${port}`);
+});
 
 module.exports = app;
