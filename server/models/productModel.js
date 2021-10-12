208e704cfc739bcb905cc8d1b924ef6016b7947a
Mariia Paraketsova <paraketsova@gmail.com>
2021-10-12 14:11:39 +0200

    Add product model

---
+++ server/models/productModel.js
@@ -0,0 +1,57 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+
+const productSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: [true, 'A product must have a name'],
+    unique: true,
+    trim: true,
+    maxlength: [
+      40,
+      'A product name must have less or equal then 40 characters',
+    ],
+    minlength: [5, 'A product name must have more or equal then 5 characters'],
+  },
+  price: { type: Number, required: [true, 'A product must have a price'] },
+  category: {
+    type: String,
+    enum: {
+      values: ['cat', 'dog'],
+      message: 'Available categories is either: cat or dog',
+    },
+  },
+  description: {
+    type: String,
+    required: [true, 'A product must have a description'],
+    maxlength: [
+      150,
+      'A product description must have less or equal then 150 characters',
+    ],
+    minlength: [
+      15,
+      'A product description must have more or equal then 15 characters',
+    ],
+  },
+  imageUrl: {
+    type: String,
+    required: [true, 'A product must have an image'],
+  },
+  weight: {
+    type: String,
+    required: [true, 'A product must have a weight'],
+  },
+  maker: {
+    type: String,
+    required: [true, 'A product must have a maker'],
+  },
+  slug: String,
+});
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+productSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+module.exports = mongoose.model('product', productSchema);
