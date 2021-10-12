09b7780ae8e5aec7c24a0ebf7f42051a9762b37d
Viktoria Ihnatsik <ignatik_viktoria@hotmail.com>
2021-10-12 14:49:03 +0200

    deleted slugify

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,34 +1,26 @@
 const mongoose = require('mongoose');
-const slugify = require('slugify');
 
 const userSchema = new mongoose.Schema({
- fullName: {
-  type: String,
-  required: [true, 'A user must have a full name'],
+   fullName: {
+   type: String,
+   required: [true, 'A user must have a full name'],
  },
  password: {
-  type: String,
-  required: [true, 'A user must have a password'],
+   type: String,
+   required: [true, 'A user must have a password'],
  },
  email: {
-  type: String,
-  required: [true, 'A user must have an email'],
+   type: String,
+   required: [true, 'A user must have an email'],
  }, 
  telefon: {
-  type: Number, 
-  required: [true, 'A user must have a telefon'],
+   type: Number, 
+   required: [true, 'A user must have a telefon'],
  },
  adress: {
-  type: String,
-  required: [true, 'A user must have an adress'],
- },
- slug: String,
-});
-
-// DOCUMENT MIDDLEWARE: runs before .save() and .create()
-productSchema.pre('save', function (next) {
-  this.slug = slugify(this.name, { lower: true });
-  next();
+   type: String,
+   required: [true, 'A user must have an adress'],
+ }
 });
 
 module.exports = mongoose.model('user', userSchema);
