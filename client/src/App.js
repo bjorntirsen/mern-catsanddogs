2765a8679e632cfb94c3729c04d163b04a73712b
rfn900 <rfn900@gmail.com>
2021-10-13 00:48:41 +0200

    Add single product route to App.js and replace one dev img

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,20 @@ import React from "react";
 import { Switch, Route } from "react-router-dom";
 import "./styles/App.css";
 import LandingPage from "./pages/LandingPage";
+import ProductDetails from "./components/ProductDetails";
 
 function App() {
-	return (
-		<div>
-			<div className="container">
-				<Switch>
-					<Route path="/" component={LandingPage} />
-				</Switch>
-			</div>
-		</div>
-	);
+  return (
+    <div>
+      <div className="container">
+        <Switch>
+          <Route path="/products/:slug" component={ProductDetails} />
+
+          <Route path="/" component={LandingPage} />
+        </Switch>
+      </div>
+    </div>
+  );
 }
 
 export default App;
