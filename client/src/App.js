b8c27b41c8e1cb7c28dad9f07bcd9c12662562bf
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-13 16:16:51 +0200

    Add Signup Page

--- client/src/App.js
+++ client/src/App.js
@@ -3,12 +3,14 @@ import { Switch, Route } from "react-router-dom";
 import "./styles/App.css";
 import LandingPage from "./pages/LandingPage";
 import ProductsListPage from "./pages/ProductsListPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
 	return (
 		<div>
 			<div className="container">
 				<Switch>
+					<Route path="/signup" component={SignUpPage} />
 
 					<Route path="/catproducts" component={ProductsListPage} />
 
