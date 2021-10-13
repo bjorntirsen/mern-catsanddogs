d1d7f756f567ca2dbee86d6858da630451ad1f3f
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-13 15:01:31 +0200

    Add Footer component and Footer style module

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,13 @@ import { Switch, Route } from "react-router-dom";
 import "./styles/App.css";
 import LandingPage from "./pages/LandingPage";
 import ProductsListPage from "./pages/ProductsListPage";
+import Footer from "./components/Footer";
 
 function App() {
 	return (
 		<div>
 			<div className="container">
 				<Switch>
-
 					<Route path="/catproducts" component={ProductsListPage} />
 
 					<Route path="/dogproducts" component={ProductsListPage} />
@@ -17,6 +17,7 @@ function App() {
 					<Route path="/" component={LandingPage} />
 				</Switch>
 			</div>
+			<Footer />
 		</div>
 	);
 }
