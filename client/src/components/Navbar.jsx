fba9cca6cfe5ddc19449b6a9bd8f644c02f9d0a1
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-14 20:39:47 +0200

    :rotating_light: Add href to anchor elements

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,63 +1,63 @@
 import React from "react";
-import logo from '../logo.png';
-import Cart from '../bxs-cart.svg';
+import logo from "../logo.png";
+import Cart from "../bxs-cart.svg";
 import { NavLink } from "react-router-dom";
-import styles from '../styles/Navbar.module.css';
+import styles from "../styles/Navbar.module.css";
 
 const Navbar = () => {
   return (
-    <div >
+    <div>
       <nav className={`${styles.nav}`}>
-          <div className={`${styles.left}`}>
-                <li className={`${styles.li}`}>
-                    <NavLink to="/">
-                        <img className={`${styles.logo}`} src={logo} alt="logo" />
-                    </NavLink>
-                </li>
-          </div>
+        <div className={`${styles.left}`}>
+          <li className={`${styles.li}`}>
+            <NavLink to="/">
+              <img className={`${styles.logo}`} src={logo} alt="logo" />
+            </NavLink>
+          </li>
+        </div>
 
-          <div className={`${styles.center}`}>
-            <ul className={`${styles.nav}`}>
-                <li className={`${styles.li}`}>
-                    <NavLink to="/">
-                        <a className={`${styles.a}`}>Home</a>
-                    </NavLink>
-                </li>
-                <li className={`${styles.li}`}>
-                    <NavLink to="/shop">
-                    <a className={`${styles.a}`}>Shop</a>
-                    </NavLink>
-                </li>
-                <li className={`${styles.li}`}>
-                    <NavLink to="/contact">
-                    <a className={`${styles.a}`}>Contact</a>
-                    </NavLink>
-                </li>
-                <li className={`${styles.li} ${styles.a}`}>
-                    <NavLink to="/about">
-                    <a className={`${styles.a}`}>About us</a>
-                    </NavLink>
-                </li>
-            </ul>
-          </div>
+        <div className={`${styles.center}`}>
+          <ul className={`${styles.nav}`}>
+            <li className={`${styles.li}`}>
+              <NavLink to="/">
+                <a href="/" className={`${styles.a}`}>Home</a>
+              </NavLink>
+            </li>
+            <li className={`${styles.li}`}>
+              <NavLink to="/shop">
+                <a href="/shop" className={`${styles.a}`}>Shop</a>
+              </NavLink>
+            </li>
+            <li className={`${styles.li}`}>
+              <NavLink to="/contact">
+                <a href="/contact" className={`${styles.a}`}>Contact</a>
+              </NavLink>
+            </li>
+            <li className={`${styles.li} ${styles.a}`}>
+              <NavLink to="/about">
+                <a href="/about" className={`${styles.a}`}>About us</a>
+              </NavLink>
+            </li>
+          </ul>
+        </div>
 
-          <div className={`${styles.right}`}>
-            <ul className={`${styles.nav}`}>
-                <li className={`${styles.li}`}>
-                    <NavLink to="/login">
-                        <a className={`${styles.a}`}>Login</a>
-                    </NavLink>
-                </li>
-                <li className={`${styles.li}`}>
-                    <NavLink to="/cart">
-                        <img className={`${styles.cart}`} src={Cart} alt="cart-icon" />
-                    </NavLink>
-                </li>
-            </ul>
-          </div>   
+        <div className={`${styles.right}`}>
+          <ul className={`${styles.nav}`}>
+            <li className={`${styles.li}`}>
+              <NavLink to="/login">
+                <a href="/login" className={`${styles.a}`}>Login</a>
+              </NavLink>
+            </li>
+            <li className={`${styles.li}`}>
+              <NavLink to="/cart">
+                <img className={`${styles.cart}`} src={Cart} alt="cart-icon" />
+              </NavLink>
+            </li>
+          </ul>
+        </div>
       </nav>
     </div>
   );
 };
- 
+
 export default Navbar;
