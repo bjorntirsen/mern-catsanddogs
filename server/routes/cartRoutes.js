4440f7b2bf2a50ff346856fe4a0af97be2768329
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-18 16:37:43 +0200

    Add cart routes create endpoint

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,12 +1,32 @@
 const express = require("express");
-//const User = require("../models/userModel");
+const User = require("../models/userModel");
+const Product = require("../models/productModel");
+
 const router = express.Router();
 
 // CREATE
-router.post("/", (req, res) => {});
+router.post("/add", async (req, res) => {
+  const { item, userId } = req.body;
+  try {
+    const product = await Product.findOne({ _id: item.id });
+    if (!product) return res.status(404).send("Product not found");
+
+    const filter = { _id: userId };
+    const update = {
+      $push: { cart: { productId: product._id, amount: item.amount } },
+    };
+
+    const updatedUser = await User.findOneAndUpdate(filter, update, {
+      new: true,
+    });
+    res.json(updatedUser);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 // READ
-router.get("/", (req, res) => {});
+router.get("/", async (req, res) => {});
 
 // UPDATE
 router.post("/:id", (req, res) => {});
