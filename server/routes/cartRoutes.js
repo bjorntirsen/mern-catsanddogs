dcf01bc3a9bf3049d5637b1cbb6c741fe6191d0e
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-18 15:36:16 +0200

    Add cart routes to app

---
+++ server/routes/cartRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+//const User = require("../models/userModel");
+const router = express.Router();
+
+// CREATE
+router.post("/", (req, res) => {});
+
+// READ
+router.get("/", (req, res) => {});
+
+// UPDATE
+router.post("/:id", (req, res) => {});
+
+// DELETE
+router.delete("/:id", (req, res) => {});
+
+module.exports = router;
