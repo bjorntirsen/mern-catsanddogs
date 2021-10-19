eb450048a3aa9aa04ce8d9506779793cda05ca65
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-19 19:37:11 +0200

    Add search functionality to SearchBar component

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,14 +1,52 @@
-import React from "react";
+import {React, useState} from "react";
+import { Link, useHistory } from "react-router-dom";
 import searchIcon from "../search-icon.svg";
 import styles from "../styles/SearchBar.module.css";
 
 export default function SearchBar({ products }) {
-  return (
-    <div className={styles.searchContainer}>
-        <div className={styles.searchBar}>
-            <input type="text" placeholder="Search..." name="search"/>
-            <button type="submit"><img className={`${styles.cart}`} src={searchIcon} alt="search-icon" /></button>
+    let history = useHistory();
+    const [searchTerm, setSearchTerm] = useState("")
+    
+    const search = (e) => {
+        e.preventDefault();
+        const filtered = products.filter(product => {
+        if (searchTerm === "") return null;
+        else if (product.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+            return product;
+        }})
+
+        if (filtered.length === 1) {
+            history.push(`/products/${filtered[0].slug}`);
+        }
+        else {
+            history.push(`/products/${searchTerm}`);
+        }
+    }
+
+    return (
+        <div className={styles.searchContainer}>
+            <div className={styles.searchBar}>
+            <input
+                autoComplete="off"
+                type="text" 
+                placeholder="Search..." 
+                name="search" 
+                onChange={e => setSearchTerm(e.target.value)}
+            />
+            <button onClick={search}><img className={`${styles.cart}`} src={searchIcon} alt="search-icon" /></button>
+            </div>
+            <ul>
+                {products.filter(product => {
+                    if (searchTerm === "") return null;
+                    else if (product.title.toLowerCase().includes(searchTerm.toLowerCase())) return product;
+                }).map((product) => {
+                    return(
+                        <Link key={product._id} to={`/products/${product.slug}`}>
+                            <li>{product.title}</li>
+                        </Link>
+                    )
+                })}
+            </ul>
         </div>
-    </div>
-  )
+    )
 }
