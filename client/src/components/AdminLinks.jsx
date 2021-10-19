83eaac5aa9cd60560b0f563aa2c3f9b3ca73b183
edvardakerman <ed@sagovagen.se>
2021-10-19 15:04:59 +0200

    add admin and user links

--- client/src/components/AdminLinks.jsx
+++ client/src/components/AdminLinks.jsx
@@ -1,9 +1,33 @@
 import React from "react";
+import styles from "../styles/Navbar.module.css";
+import Cart from "../bxs-cart.svg";
+import { NavLink } from "react-router-dom";
 
 const AdminLinks = () => {
   return (
-    <div>
-      <p>Admin Links Placeholder</p>
+    <div className={`${styles.right}`}>
+      <ul className={`${styles.nav}`}>
+        <li className={`${styles.li}`}>
+          <NavLink to={`/admin`}>
+            <span className={`${styles.a}`}>Admin</span>
+          </NavLink>
+        </li>
+        <li className={`${styles.li}`}>
+          <NavLink to={`/logout`}>
+            <span className={`${styles.a}`}>Logout</span>
+          </NavLink>
+        </li>
+        <li className={`${styles.li}`}>
+          <NavLink to={`/getme`}>
+            <span className={`${styles.a}`}>Get Me</span>
+          </NavLink>
+        </li>
+        <li className={`${styles.li}`}>
+          <NavLink to="/cart">
+            <img className={`${styles.cart}`} src={Cart} alt="cart-icon" />
+          </NavLink>
+        </li>
+      </ul>
     </div>
   );
 };
