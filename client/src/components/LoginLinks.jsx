7abdf0a59b111bb0050ab8a035cbb0ec2eb95faf
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-19 15:52:52 +0200

    :sparkles: Add logout handler

--- client/src/components/LoginLinks.jsx
+++ client/src/components/LoginLinks.jsx
@@ -8,13 +8,13 @@ const LoginLinks = () => {
     <div className={styles.right}>
       <ul className={styles.nav}>
         <li className={styles.li}>
-          <NavLink to={`/login`}>
-            <span className={styles.a}>Login</span>
+          <NavLink to={`/login`} className={styles.a}>
+            Login
           </NavLink>
         </li>
         <li className={styles.li}>
-          <NavLink to={`/signup`}>
-            <span className={styles.a}>Sign Up!</span>
+          <NavLink to={`/signup`} className={styles.a}>
+            Sign Up
           </NavLink>
         </li>
         <li className={styles.li}>
