7fa12f8641830b8ee18d9a70527bb86310c41ce8
Mariia Paraketsova <paraketsova@gmail.com>
2021-10-19 20:03:23 +0200

    Add AdminProductEditp age(with form submit without POST to DB). Fix AdminProductsPage. Edit route name foe AdminProductsPage

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import SignUpPage from "./pages/SignUpPage";
 import LoginPage from "./pages/LoginPage";
 import ShoppingCartPage from "./pages/ShoppingCartPage";
 import AdminProductsPage from "./pages/AdminProductsPage";
+import AdminEditProductPage from "./pages/AdminEditProductPage";
 import Footer from "./components/Footer";
 
 function App() {
@@ -20,7 +21,9 @@ function App() {
           <Route path="/cart" component={ShoppingCartPage} />
           <Route path="/signup" component={SignUpPage} />
           <Route path="/login" component={LoginPage} />
-          <Route path="/admin" component={AdminProductsPage} />
+          <Route path="/admin_products" component={AdminProductsPage} />
+          <Route path="/admin_product/:slug" component={AdminEditProductPage} />
+
           <Route
             path="/products/categories/:category"
             component={ProductsListPage}
---
+++ client/src/pages/AdminEditProductPage.jsx
@@ -0,0 +1,113 @@
+import { React, useState, useEffect } from "react";
+import styles from "../styles/AdminEditProduct.module.css";
+
+const AdminEditProduct = ({ match }) => {
+  const [products, setProducts] = useState(null);
+  const [product, setProduct] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
+
+  const slug = match.url.split("/")[2];
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const url = "/api/products";
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const responseData = await response.json();
+      setProducts(responseData.data.products);
+      setIsLoading(false);
+    };
+    fetchProducts().catch((error) => {
+      setIsLoading(false);
+      setErrorMessage(error.message);
+    });
+  }, [match]);
+
+  useEffect(() => {
+    if (products) {
+      const [item] = products.filter((item) => item.slug === slug);
+      if (!item)
+        return setErrorMessage(
+          "No item with that slug was found in the database"
+        );
+      setProduct(item);
+    }
+  }, [products, slug]);
+
+  function renderInputs(keyList) {
+    return Object.entries(product)
+      .filter(([k, v]) => keyList.includes(k))
+      .map(([k, v]) => (
+        <tr key={k}>
+          <th className={styles.th}>{k}</th>
+          <td>
+            <input className={styles.input}
+              type="text"
+              name={k}
+              value={v || ''}
+              size={50}
+              onChange={getHandleChange(k)}
+            />
+          </td>
+        </tr>
+      ));
+  }
+
+  function handleOnSubmit(e) { //TODO - add function to edit product in DB
+    e.preventDefault()
+  }
+
+  const getHandleChange = (k) => (event) => {
+    setProduct({ ...product, [k]: event.target.value });
+  };
+
+  if (isLoading) {
+    return (
+      <section className={styles.IsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (errorMessage) {
+    return (
+
+      <section className={styles.ErrorMessage}>
+        <p>{errorMessage}</p>
+      </section>
+    );
+  }
+
+  return (
+    <div className={styles.body}>
+      <div className={styles.ap_container}>
+        <h2 className={styles.header}>Admin Page</h2>
+        <h3 className={styles.header}>Edit product</h3>
+        <form onSubmit={handleOnSubmit}>
+          <table className={styles.ap_table}>
+            <tbody>
+              {renderInputs([
+                "title",
+                "price",
+                "categoty",
+                "description",
+                "imageUrl",
+                "weight",
+                "maker",
+                "stock"
+              ])}
+            </tbody>
+          </table>
+          <button type="submit">Update product</button>
+        </form>
+      </div>
+    </div>
+  );
+
+};
+
+
+export default AdminEditProduct;
