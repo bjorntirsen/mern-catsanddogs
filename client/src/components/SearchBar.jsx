99749683e5229a6d681c8c8d55081a9731fdfa7c
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-19 15:43:17 +0200

    Add search button and styling

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import styles from "../styles/SearchBar.module.css"; // Import css modules stylesheet as styles
+import searchIcon from "../search-icon.svg";
+import styles from "../styles/SearchBar.module.css";
 
 export default function SearchBar({ products }) {
   return (
-    <div className={styles.searchBar}>
-        <input type="text" placeholder="Search..." />
+    <div className={styles.searchContainer}>
+        <div className={styles.searchBar}>
+            <input type="text" placeholder="Search..." name="search"/>
+            <button type="submit"><img className={`${styles.cart}`} src={searchIcon} alt="search-icon" /></button>
+        </div>
     </div>
   )
 }
