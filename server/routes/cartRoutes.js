d633af10c24c49ef7b8b8c1eff077142aca3d04a
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-20 15:02:21 +0200

    :construction: Some exprerimental code

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,9 +1,91 @@
 const express = require("express");
+const { ObjectId } = require("mongoose").Types;
 const User = require("../models/userModel");
 const Product = require("../models/productModel");
 const { protect } = require("../controllers/authControllers");
 
 const router = express.Router();
+
+// Helper function
+// const containsInvalidProducts = async (updateToCart) => {
+//   let invalidProducts = false;
+//   const productsPromises = [];
+//   updateToCart.forEach((orderItem) => {
+//     productsPromises.push(
+//       ObjectId.isValid(orderItem.productId)
+//         ? Product.findById(orderItem.productId).exec()
+//         : null
+//     );
+
+//     const missingProductId =
+//       orderItem.productId === "" || typeof orderItem.productId === "undefined";
+
+//     const missingAmount = typeof orderItem.amount === "undefined";
+
+//     invalidProducts = invalidProducts || missingAmount || missingProductId;
+//   });
+
+//   const arrayOfProducts = await Promise.all(productsPromises);
+//   const isMissingProducts = arrayOfProducts.includes(null);
+
+//   missingOrderItem = missingOrderItem || isMissingProducts;
+
+//   return missingOrderItem;
+// };
+
+// CRUD operations
+// (Create) Add one item to cart
+router.post("/addOne", protect, async (req, res, next) => {
+  try {
+    if (!req.body.id) {
+      return res
+        .status(401)
+        .json(
+          "You need to provide the id of the product you wish to add to cart."
+        );
+    }
+    const product = await Product.findById(req.body.id);
+    if (!product) return res.status(400).json("Invalid product id.");
+    const user = await User.findById(req.user._id);
+    // If the cart is empty
+    if (user.cart.length === 0) {
+      const filter = { _id: req.user };
+      const update = {
+        $push: { cart: { productId: product._id, amount: 1 } },
+      };
+
+      const updatedUser = await User.findOneAndUpdate(filter, update, {
+        new: true,
+      });
+      return res.json(updatedUser);
+    }
+    // If the item is already in cart
+    if (
+      user.cart.filter((item) => item.productId.equals(ObjectId(req.body.id)))
+        .length > 0
+    ) {
+      const subdocument = user.cart.find((item) =>
+        item.productId.equals(ObjectId(req.body.id))
+      );
+      console.log(user.cart);
+      console.log(subdocument._id);
+      const filter = { _id: req.user, "cart._id": subdocument._id };
+      const update = {
+        $inc: { "cart.$.amount": 1 },
+      };
+      const updatedUser = await User.findOneAndUpdate(filter, update, {
+        new: true,
+      });
+      res.status(200).json({
+        status: "success",
+        cart: updatedUser.cart,
+      });
+    }
+    return res.json(user);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+});
 
 // Add product + amount to user's cart
 router.post("/update", protect, async (req, res) => {
