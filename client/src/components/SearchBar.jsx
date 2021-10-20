07430529c057421acf10a775bebb8128e91252fc
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-20 08:52:58 +0200

    :rotating_light: Fix linting & formatting

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,52 +1,65 @@
-import {React, useState} from "react";
+import { React, useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import searchIcon from "../search-icon.svg";
 import styles from "../styles/SearchBar.module.css";
 
 export default function SearchBar({ products }) {
-    let history = useHistory();
-    const [searchTerm, setSearchTerm] = useState("")
-    
-    const search = (e) => {
-        e.preventDefault();
-        const filtered = products.filter(product => {
-        if (searchTerm === "") return null;
-        else if (product.title.toLowerCase().includes(searchTerm.toLowerCase())) {
-            return product;
-        }})
+  let history = useHistory();
+  const [searchTerm, setSearchTerm] = useState("");
 
-        if (filtered.length === 1) {
-            history.push(`/products/${filtered[0].slug}`);
-        }
-        else {
-            history.push(`/products/${searchTerm}`);
-        }
+  const search = (e) => {
+    e.preventDefault();
+    const filtered = products.filter((product) => {
+      if (searchTerm === "") return null;
+      else if (product.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+        return product;
+      }
+      return null;
+    });
+
+    if (filtered.length === 1) {
+      history.push(`/products/${filtered[0].slug}`);
+    } else {
+      history.push(`/products/${searchTerm}`);
     }
+  };
 
-    return (
-        <div className={styles.searchContainer}>
-            <div className={styles.searchBar}>
-            <input
-                autoComplete="off"
-                type="text" 
-                placeholder="Search..." 
-                name="search" 
-                onChange={e => setSearchTerm(e.target.value)}
-            />
-            <button onClick={search}><img className={`${styles.cart}`} src={searchIcon} alt="search-icon" /></button>
-            </div>
-            <ul>
-                {products.filter(product => {
-                    if (searchTerm === "") return null;
-                    else if (product.title.toLowerCase().includes(searchTerm.toLowerCase())) return product;
-                }).map((product) => {
-                    return(
-                        <Link key={product._id} to={`/products/${product.slug}`}>
-                            <li>{product.title}</li>
-                        </Link>
-                    )
-                })}
-            </ul>
-        </div>
-    )
+  return (
+    <div className={styles.searchContainer}>
+      <div className={styles.searchBar}>
+        <input
+          autoComplete="off"
+          type="text"
+          placeholder="Search..."
+          name="search"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+        <button onClick={search}>
+          <img
+            className={`${styles.cart}`}
+            src={searchIcon}
+            alt="search-icon"
+          />
+        </button>
+      </div>
+      <ul>
+        {products
+          .filter((product) => {
+            if (searchTerm === "") return null;
+            else if (
+              product.title.toLowerCase().includes(searchTerm.toLowerCase())
+            )
+              return product;
+            return null;
+          })
+          .map((product) => {
+            return (
+              <Link key={product._id} to={`/products/${product.slug}`}>
+                <li>{product.title}</li>
+              </Link>
+            );
+          })}
+      </ul>
+    </div>
+  );
 }
