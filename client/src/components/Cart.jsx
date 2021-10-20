de9f43a1b287a56d324fa96365ed6a087e0c756d
Björn Tirsén <bjorntirsen@gmail.com>
2021-10-20 21:28:47 +0200

    :sparkles: Render users cart dynamically

--- client/src/components/Cart.jsx
+++ client/src/components/Cart.jsx
@@ -1,36 +1,89 @@
-import React from "react";
-
-import Button from "./Button";
-
+import React, { useContext, useEffect, useState } from "react";
+import { UserContext } from "../contexts/UserContext";
 import styles from "../styles/Cart.module.css";
+import Button from "./Button";
 import CartItem from "./CartItem";
 
 const Cart = () => {
-  return (
-    <section className={styles.body}>
-      <div className={styles.cart_container}>
-        <div className={styles.header}>
-          <h2>Shopping Cart</h2>
-          <h5 className={styles.action}>Remove all</h5>
-        </div>
+  const [products, setProducts] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
+  const { user } = useContext(UserContext);
 
-        <CartItem />
-        <CartItem />
-        <CartItem />
+  useEffect(() => {
+    const fetchProducts = async () => {
+      const url = "/api/products";
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+      const responseData = await response.json();
+      setProducts(responseData.data.products);
+      setIsLoading(false);
+    };
+    fetchProducts().catch((error) => {
+      setIsLoading(false);
+      setErrorMessage(error.message);
+    });
+  }, []);
 
-        <div className={styles.summary}>
-          <div className={styles.total}>
-            <div>
-              <div className={styles.totalprice}>Total</div>
-              <div className={styles.items}>2 items</div>
+  if (isLoading) {
+    return (
+      <section className={styles.IsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (errorMessage) {
+    return (
+      <section className={styles.ErrorMessage}>
+        <p>{errorMessage}</p>
+      </section>
+    );
+  }
+
+  if (!isLoading && user)
+    return (
+      <section className={styles.body}>
+        <div className={styles.cart_container}>
+          <div className={styles.header}>
+            <h2>Shopping Cart</h2>
+            <h5 className={styles.action}>Remove all</h5>
+          </div>
+
+          {user.cart.map((product) => {
+            const fullProduct = products.find((item) => {
+              return item._id === product.productId;
+            });
+            return (
+              <CartItem
+                key={product.productId}
+                product={fullProduct}
+                amount={product.amount}
+              />
+            );
+          })}
+
+          <div className={styles.summary}>
+            <div className={styles.total}>
+              <div>
+                <div className={styles.totalprice}>Total</div>
+                <div className={styles.items}>2 items</div>
+              </div>
+              <div className={styles.total_amount}>$8.98</div>
             </div>
-            <div className={styles.total_amount}>$8.98</div>
-          </div>
-          <div className={styles.button}>
-            <Button type="primary" text="Checkout" />
+            <div className={styles.button}>
+              <Button type="primary" text="Checkout" />
+            </div>
           </div>
         </div>
-      </div>
+      </section>
+    );
+
+  return (
+    <section className={styles.ErrorMessage}>
+      <p>"Something went wrong!"</p>
     </section>
   );
 };
--- client/src/components/CartItem.jsx
+++ client/src/components/CartItem.jsx
@@ -1,25 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 
 import styles from "../styles/CartItem.module.css";
 
-const CartItem = () => {
+const CartItem = ({ product, amount }) => {
+  const [quantity, setQuantity] = useState(amount);
+
+  const reduceQuantityHandler = () => {
+    quantity > 1 && setQuantity(quantity - 1);
+  };
+
+  const increaseQuantityHandler = () => {
+    setQuantity(quantity + 1);
+  };
+
+  const onChangeHandler = (e) => {
+    setQuantity(parseInt(e.target.value));
+  };
+
   return (
     <div className={styles.cart_items}>
       <div className={styles.image_box}>
         <img
           className={styles.img}
-          src="https://static.zara.net/photos///2021/I/0/1/p/0653/289/712/2/w/1126/0653289712_2_1_1.jpg?ts=1632311345187"
-          alt="product"
+          src={product.imageUrl}
+          alt={product.title}
         />
       </div>
-      <h1 className={styles.title}>Product Name</h1>
+      <h1 className={styles.title}>{product.title}</h1>
       <div className={styles.counter}>
-        <div className={styles.btn}>+</div>
-        <div className={styles.count}>1</div>
-        <div className={styles.btn}>-</div>
+        <span className={styles.btn} onClick={reduceQuantityHandler}>
+          -
+        </span>
+        <input
+          className={styles.count}
+          onChange={onChangeHandler}
+          type="number"
+          name="qty_input"
+          value={quantity}
+          min="1"
+        />
+        <span className={styles.btn} onClick={increaseQuantityHandler}>
+          +
+        </span>
       </div>
       <div className={styles.prices}>
-        <div className={styles.amount}>$2.99</div>
+        <div className={styles.amount}>${product.price}</div>
         <div className={styles.remove}>Remove</div>
       </div>
     </div>
