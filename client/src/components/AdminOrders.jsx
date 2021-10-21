fd2d8694c99a9560fec9b6ac26ab240f12b1b3b8
edvardakerman <ed@sagovagen.se>
2021-10-21 10:26:20 +0200

    setup fetch and log orders

--- client/src/components/AdminOrders.jsx
+++ client/src/components/AdminOrders.jsx
@@ -1,6 +1,57 @@
-import { React } from "react";
+import { React, useEffect, useState } from "react";
 
 const AdminOrders = () => {
+  const [orders, setOrders] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [errorMessage, setErrorMessage] = useState();
+
+  useEffect(() => {
+    const fetchOrders = async () => {
+      const url = "/api/orders";
+      const token = localStorage.getItem("tkn");
+      const obj = {
+        headers: {
+          authorization: `Bearer ${token}`,
+        },
+      };
+
+      const response = await fetch(url, obj);
+
+      if (!response.ok) {
+        throw new Error("Something went wrong!");
+      }
+
+      const responseData = await response.json();
+
+      console.log(responseData);
+
+      setOrders(responseData.data.orders);
+      setIsLoading(false);
+    };
+    fetchOrders().catch((error) => {
+      setIsLoading(false);
+      setErrorMessage(error.message);
+    });
+  }, []);
+
+  console.log(orders);
+
+  if (isLoading) {
+    return (
+      <section>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (errorMessage) {
+    return (
+      <section>
+        <p>{errorMessage}</p>
+      </section>
+    );
+  }
+
   return (
     <div>
       <h1>Admin Orders Page</h1>
