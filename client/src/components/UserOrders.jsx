be9b7cccca69486720dfc2e01d6bfa17fd7c1778
edvardakerman <ed@sagovagen.se>
2021-10-21 13:04:26 +0200

    setup router and UserOrdersPage

---
+++ client/src/components/UserOrders.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UserOrders = () => {
+  return (
+    <div>
+      <p>Profile Order Page</p>
+    </div>
+  );
+};
+
+export default UserOrders;
---
+++ client/src/pages/UserOrdersPage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function UserOrdersPage() {
+  return (
+    <div>
+      <p>User Order Page</p>
+    </div>
+  );
+}
