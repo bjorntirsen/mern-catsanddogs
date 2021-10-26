f667596b5b398f3438d35f8765a376e5e2bd5685
Mariia Paraketsova <paraketsova@gmail.com>
2021-10-26 13:49:12 +0200

    Add to admins cart #

--- client/src/components/AdminLinks.jsx
+++ client/src/components/AdminLinks.jsx
@@ -1,11 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import styles from "../styles/Navbar.module.css";
 import Cart from "../bxs-cart.svg";
 import { NavLink } from "react-router-dom";
 import { UserContext } from "../contexts/UserContext";
 
 const AdminLinks = ({ logoutHandler }) => {
+  const [cartSum, setCartSum] = useState(null);
   const { user } = useContext(UserContext);
+
+  useEffect(() => {
+    if (user) {
+      let sum = user.cart
+        .map((item) => item.amount)
+        .reduce((previousValue, currentValue) => {
+          return previousValue + currentValue;
+        }, 0);
+      setCartSum(sum);
+    }
+  }, [user]);
 
   return (
     <div className={styles.right}>
@@ -35,10 +47,11 @@ const AdminLinks = ({ logoutHandler }) => {
             Logout
           </span>
         </li>
-        <li className={styles.li}>
+        <li className={`${styles.li} ${styles.cartLi}`}>
           <NavLink to="/cart">
             <img className={styles.cart} src={Cart} alt="cart-icon" />
           </NavLink>
+          <span>{cartSum}</span>
         </li>
       </ul>
     </div>
