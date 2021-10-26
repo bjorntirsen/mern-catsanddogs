aff99863a6907643524ab3d765b8ed6d467f58b0
Mariia Paraketsova <paraketsova@gmail.com>
2021-10-26 19:39:27 +0200

    Refactor: move cart controller functions into cartController

--- server/routes/cartRoutes.js
+++ server/routes/cartRoutes.js
@@ -1,216 +1,24 @@
 const express = require("express");
-const { ObjectId } = require("mongoose").Types;
-
-const User = require("../models/userModel");
-const Product = require("../models/productModel");
 const { protect } = require("../controllers/authControllers");
+
+const {
+  createCartItem,
+  createCartItems,
+  getUserCart,
+  updateCart,
+  emptyCart,
+} = require("../controllers/cartControllers");
 
 const router = express.Router();
 
-// CRUD operations
-// (Create) Add one item to cart
-router.post("/addOne", protect, async (req, res, next) => {
-  try {
-    if (!req.body.id) {
-      return res
-        .status(401)
-        .json(
-          "You need to provide the id of the product you wish to add to cart."
-        );
-    }
-    const product = await Product.findById(req.body.id);
-    if (!product) return res.status(400).json("Invalid product id.");
-    const user = await User.findById(req.user._id);
-    // If the cart is empty or item is not in cart
-    if (
-      user.cart.length === 0 ||
-      user.cart.filter((item) => item.productId.equals(ObjectId(req.body.id)))
-        .length === 0
-    ) {
-      const filter = { _id: req.user };
-      const update = {
-        $push: { cart: { productId: product._id, amount: 1 } },
-      };
+router.post("/addOne", protect, createCartItem);
 
-      const updatedUser = await User.findOneAndUpdate(filter, update, {
-        new: true,
-      });
-      return res.status(200).json({
-        status: "success",
-        data: {
-          user: updatedUser,
-        },
-      });
-    }
-    // If the item is already in cart
-    if (
-      user.cart.filter((item) => item.productId.equals(ObjectId(req.body.id)))
-        .length > 0
-    ) {
-      const subdocument = user.cart.find((item) =>
-        item.productId.equals(ObjectId(req.body.id))
-      );
-      const filter = { _id: req.user, "cart._id": subdocument._id };
-      const update = {
-        $inc: { "cart.$.amount": 1 },
-      };
-      const updatedUser = await User.findOneAndUpdate(filter, update, {
-        new: true,
-      });
-      return res.status(200).json({
-        status: "success",
-        data: {
-          user: updatedUser,
-        },
-      });
-    }
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+router.post("/addMany", protect, createCartItems);
 
-// (Create) Add many of one item to cart
-router.post("/addMany", protect, async (req, res, next) => {
-  try {
-    if (!req.body.id || !req.body.amount) {
-      return res
-        .status(401)
-        .json(
-          "You need to provide the id of the product and the amount you wish to add to cart."
-        );
-    }
-    const product = await Product.findById(req.body.id);
-    if (!product) return res.status(400).json("Invalid product id.");
-    const user = await User.findById(req.user._id);
-    // If the cart is empty or item is not in cart
-    if (
-      user.cart.length === 0 ||
-      user.cart.filter((item) => item.productId.equals(ObjectId(req.body.id)))
-        .length === 0
-    ) {
-      const filter = { _id: req.user };
-      const update = {
-        $push: { cart: { productId: product._id, amount: +req.body.amount } },
-      };
+router.get("/", protect, getUserCart);
 
-      const updatedUser = await User.findOneAndUpdate(filter, update, {
-        new: true,
-      });
-      return res.status(200).json({
-        status: "success",
-        data: {
-          user: updatedUser,
-        },
-      });
-    }
-    // If the item is already in cart
-    if (
-      user.cart.filter((item) => item.productId.equals(ObjectId(req.body.id)))
-        .length > 0
-    ) {
-      const subdocument = user.cart.find((item) =>
-        item.productId.equals(ObjectId(req.body.id))
-      );
-      const filter = { _id: req.user, "cart._id": subdocument._id };
-      const update = {
-        $inc: { "cart.$.amount": +req.body.amount },
-      };
-      const updatedUser = await User.findOneAndUpdate(filter, update, {
-        new: true,
-      });
-      return res.status(200).json({
-        status: "success",
-        data: {
-          user: updatedUser,
-        },
-      });
-    }
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+router.post("/update", protect, updateCart);
 
-// Get user's cart!
-router.get("/", protect, async (req, res) => {
-  const userId = req.user;
-  try {
-    const user = await User.findOne({ _id: userId });
-    if (!user) return res.status(404).json("User not found");
-    const { cart } = user;
-    res.status(200).json({
-      status: "success",
-      cart,
-    });
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
-
-// Helper function to see if cart is valid
-const cartIsValid = async (updatedCart) => {
-  const productsPromises = [];
-  updatedCart.forEach((orderItem) => {
-    productsPromises.push(
-      ObjectId.isValid(orderItem.productId)
-        ? Product.findById(orderItem.productId).exec()
-        : null
-    );
-  });
-
-  const arrayOfProducts = await Promise.all(productsPromises);
-
-  return !arrayOfProducts.some((el) => el === null);
-};
-
-// Update the entire cart
-router.post("/update", protect, async (req, res, next) => {
-  try {
-    const { updatedCart } = req.body;
-    const validCart = await cartIsValid(updatedCart);
-    if (!validCart) {
-      return res
-        .status(401)
-        .json("One or more items in your cart are not valid products.");
-    }
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: req.user._id },
-      {
-        $set: { cart: updatedCart },
-      },
-      {
-        new: true,
-      }
-    );
-    return res.status(200).json({
-      status: "success",
-      data: {
-        user: updatedUser,
-      },
-    });
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
-
-// (Delete) Empty the entire cart
-router.delete("/emptyCart", protect, async (req, res) => {
-  try {
-    const filter = { _id: req.user._id };
-    const update = {
-      $set: { cart: [] },
-    };
-    const updatedUser = await User.findOneAndUpdate(filter, update, {
-      new: true,
-    });
-    res.status(200).json({
-      status: "success",
-      data: {
-        user: updatedUser,
-      },
-    });
-  } catch (error) {
-    res.status(400).json(error);
-  }
-});
+router.delete("/emptyCart", protect, emptyCart);
 
 module.exports = router;
