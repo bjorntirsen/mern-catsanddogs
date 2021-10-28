c39a27510879bd574492b450db7e4fdc1c801614
Marta Pettersson <marta.pettersson@gmail.com>
2021-10-28 13:26:51 +0200

    Refactor authController with catchAsync and AppError.

--- server/controllers/authControllers.js
+++ server/controllers/authControllers.js
@@ -1,51 +1,45 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+const AppError = require("../utils/AppError");
+
 // MIDDLEWARE
-const protect = async (req, res, next) => {
-  try {
-    // 1) Check if token exists
-    let token;
-    if (
-      req.headers.authorization &&
-      req.headers.authorization.startsWith("Bearer")
-    ) {
-      token = req.headers.authorization.split(" ")[1];
-    }
-    if (!token) {
-      return res.status(401).json("You are not logged in. Log in to access.");
-    }
-
-    // 2) Validate token
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-    // 3) Check if user still exists
-    const currentUser = await User.findById(decoded.userId);
-    if (!currentUser) {
-      return res
-        .status(401)
-        .json("The user belonging to the token does no longer exist.");
-    }
-    // Grant access
-    req.user = currentUser;
-    //res.locals.user = currentUser;
-    next();
-  } catch (err) {
-    res.status(400).json(err);
+const protect = catchAsync(async (req, res, next) => {
+  // 1) Check if token exists
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith("Bearer")
+  ) {
+    token = req.headers.authorization.split(" ")[1];
   }
-};
+  if (!token) {
+    return next(new AppError("You are not logged in. Log in to access.", 401));
+  }
+
+  // 2) Validate token
+  const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+  // 3) Check if user still exists
+  const currentUser = await User.findById(decoded.userId);
+  if (!currentUser) {
+    return next(
+      new AppError("The user belonging to the token does no longer exist.", 401)
+    );
+  }
+  // Grant access
+  req.user = currentUser;
+  next();
+});
 
 // Must have protect before it in stack
-const restrictToAdmin = async (req, res, next) => {
-  try {
-    if (!req.user.adminUser) {
-      return res
-        .status(403)
-        .json("You do not have permission to perform this action");
-    }
-    next();
-  } catch (err) {
-    return res.status(400).json(err);
+const restrictToAdmin = catchAsync(async (req, res, next) => {
+  if (!req.user.adminUser) {
+    return next(
+      new AppError("You do not have permission to perform this action.", 403)
+    );
   }
-};
+  next();
+});
 
 module.exports = { protect, restrictToAdmin };
